b75e0a46fda74557eb89ae59539a09c81875b113
pandawaa <pallakangoding@gmail.com>
2024-01-11 15:00:14 +0800

    feat: Home Page

---
+++ components/CustomButton.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import { CustomButtonProps } from "@/types"
+import Image from "next/image"
+
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+}: CustomButtonProps) => {
+  return (
+    <button
+      disabled={false}
+      type={"button"}
+      className={`custom-btn ${containerStyles}`}
+      onClick={() => {}}
+    >
+      <span className={`flex-1`}>Explore Cars</span>
+    </button>
+  )
+}
+
+export default CustomButton
